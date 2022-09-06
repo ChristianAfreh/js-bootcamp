699364c6ee3c21fc3ad2568175c3fceb6a463a2d
Christian Afreh <afrehchristian@gmail.com>
2022-09-06 11:51:07 +0000

    added xmr

--- hangman/app.js
+++ hangman/app.js
@@ -10,7 +10,7 @@
 
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('car parts',2)
+const game1 = new Hangman('car parts', 2)
 
 puzzleEl.textContent = game1.Puzzle
 guessesEl.textContent = game1.StatusMessage
@@ -19,12 +19,52 @@ guessesEl.textContent = game1.StatusMessage
 // game1.getStatusMessage()
 
 
-window.addEventListener('keypress',function(e){
+window.addEventListener('keypress', (e) => {
     //tutorial used e.charCode(deprecated) and String.fromCharCode() method --> I used e.key instead
     const guess = e.key
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.Puzzle
-    guessesEl.textContent =  game1.StatusMessage
-   
-   
+    guessesEl.textContent = game1.StatusMessage
+
+
 })
+
+//Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place.')
+    }
+})
+
+// https://puzzle.mead.io/puzzle -- for random phrases
+// https://restcountries.com/v2/all -- for countries
+request.open('GET', 'https://restcountries.com/v2/all')
+request.send()
+
+//Challenge
+// 1. Make a new request for all countries
+// 2. Parse the responseText to get back the array of objects
+// 3. Find your country object by its country code(alpha property)
+// 4. Print the full country name(name property)
+
+const countryCode = "GH"
+
+const request1 = new XMLHttpRequest()
+
+request1.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((item) => item.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place.')
+    }
+})
+
+request1.open('GET', 'https://restcountries.com/v2/all')
+request1.send()
