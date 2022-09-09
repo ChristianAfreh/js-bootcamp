f2a0eade8ec26afdee89ba9d0594efdd65a7297c
Christian Afreh <afrehchristian@gmail.com>
2022-09-09 20:08:39 +0000

    added styling to todo app

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -100,26 +100,30 @@ const filters1 = {
 
 renderTodos(todos1, filters1)
 
-document.querySelector('#new-todo').addEventListener('input',  (e) => {
+document.querySelector('#new-todo').addEventListener('input', (e) => {
     filters1.searchText = e.target.value
     renderTodos(todos1, filters1)
 })
 
 
-document.querySelector('#todo-form').addEventListener('submit',  (e) => {
+document.querySelector('#todo-form').addEventListener('submit', (e) => {
     e.preventDefault()
-    todos1.push({
-        id: uuidv4(),
-        text: e.target.elements.addTodo.value,
-        completed: false
-    })
-    saveTodos(todos1)
-    renderTodos(todos1, filters1)
-    e.target.elements.addTodo.value = ''
+    const text = e.target.elements.addTodo.value.trim()
+    if (text.length > 0) {
+        todos1.push({
+            id: uuidv4(),
+            text: text,
+            completed: false
+        })
+        saveTodos(todos1)
+        renderTodos(todos1, filters1)
+        e.target.elements.addTodo.value = ''
+    }
+
 })
 
 
-document.querySelector('#hide-completed').addEventListener('change',  (e) => {
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
     filters1.hideCompleted = e.target.checked
     renderTodos(todos1, filters1)
 
